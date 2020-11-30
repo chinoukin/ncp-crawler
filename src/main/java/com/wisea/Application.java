1a8a7a4d7b9c02e0fc204a31d80a008739d383f2
chinoukin <chinoukin@qq.com>
2020-11-30 11:42:12 +0800

    support https or http at the same time

--- src/main/java/com/wisea/Application.java
+++ src/main/java/com/wisea/Application.java
@@ -1,8 +1,12 @@
 package com.wisea;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @MapperScan(basePackages = "com.wisea.mapper")
 public class Application {
+    @Value("${server.http.port}")
+    private int httpPort;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -24,6 +30,19 @@ public class Application {
         requestFactory.setReadTimeout(10000);
         RestTemplate restTemplate = new RestTemplate(requestFactory);
         return restTemplate;
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
     }
 
 
