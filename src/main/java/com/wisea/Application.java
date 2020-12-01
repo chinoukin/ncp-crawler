fba2d958770753a1bec3d7afe09b0bc4c81e8fbd
chinoukin <chinoukin@qq.com>
2020-12-01 10:10:39 +0800

    add undertow server for test[Caused by: java.io.IOException: Connection reset by peer ]

--- src/main/java/com/wisea/Application.java
+++ src/main/java/com/wisea/Application.java
@@ -1,11 +1,16 @@
 package com.wisea;
 
-import org.apache.catalina.connector.Connector;
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import io.undertow.servlet.api.SecurityConstraint;
+import io.undertow.servlet.api.SecurityInfo;
+import io.undertow.servlet.api.TransportGuaranteeType;
+import io.undertow.servlet.api.WebResourceCollection;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -18,6 +23,8 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
     @Value("${server.http.port}")
     private int httpPort;
+    @Value("${server.port}")
+    private int httpsPort;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -32,17 +39,34 @@ public class Application {
         return restTemplate;
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
-        return tomcat;
-    }
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(httpPort);
+//        tomcat.addAdditionalTomcatConnectors(connector); // 添加http
+//        return tomcat;
+//    }
 
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(httpPort);
-        return connector;
+
+    @Bean
+    public ServletWebServerFactory undertowFactory() {
+        UndertowServletWebServerFactory undertowFactory = new UndertowServletWebServerFactory();
+        undertowFactory.addBuilderCustomizers((Undertow.Builder builder) -> {
+            builder.addHttpListener(httpPort, "0.0.0.0");
+            // 开启HTTP2
+            builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+        });
+
+//        undertowFactory.addDeploymentInfoCustomizers(deploymentInfo -> {
+//            // 开启HTTP自动跳转至HTTPS
+//            deploymentInfo.addSecurityConstraint(new SecurityConstraint()
+//                    .addWebResourceCollection(new WebResourceCollection().addUrlPattern("/*"))
+//                    .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+//                    .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
+//                    .setConfidentialPortManager(exchange -> httpsPort);
+//        });
+        return undertowFactory;
     }
 
 
