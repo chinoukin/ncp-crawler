e6e57c298d0113b41af0d8aa0d2037e1e68f54e4
chinoukin <chinoukin@qq.com>
2020-12-15 12:00:06 +0800

    add a request for authorization code

--- src/main/java/com/wisea/controller/AbstractController.java
+++ src/main/java/com/wisea/controller/AbstractController.java
@@ -6,6 +6,7 @@ import org.springframework.boot.origin.OriginTrackedValue;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,22 +41,42 @@ public class AbstractController {
     @GetMapping(value = {"/env", "/env1", "/env2"})
     public Map<String, Object> env() {
         System.out.println(environment.getDefaultProfiles()[0]);
+
         StandardEnvironment standardEnvironment = (StandardEnvironment) environment;
         MutablePropertySources mutablePropertySources = standardEnvironment.getPropertySources();
         Iterator<PropertySource<?>> iterator = mutablePropertySources.iterator();
         //Map<String, Object> map = new HashMap<>();
         Map<String, Object> map = new TreeMap<>();
+
+        OriginTrackedMapPropertySource originTrackedMapPropertySource = null;
+        SimpleCommandLinePropertySource simpleCommandLinePropertySource = null;
+
         while (iterator.hasNext()) {
             PropertySource<?> propertySource = iterator.next();
             if (propertySource instanceof OriginTrackedMapPropertySource) {
-                OriginTrackedMapPropertySource originTrackedMapPropertySource = (OriginTrackedMapPropertySource) propertySource;
-                Map<String, Object> source = originTrackedMapPropertySource.getSource();
-                for (String key : source.keySet()) {
-                    OriginTrackedValue originTrackedValue = (OriginTrackedValue) source.get(key);
-                    map.put(key, originTrackedValue.getValue());
-                }
+                originTrackedMapPropertySource = (OriginTrackedMapPropertySource) propertySource;
+            }
+            // 命令行参数，判断有没有覆盖配置文件参数
+            if (propertySource instanceof SimpleCommandLinePropertySource) {
+                simpleCommandLinePropertySource = (SimpleCommandLinePropertySource) propertySource;
             }
         }
+
+        Map<String, Object> source = originTrackedMapPropertySource.getSource();
+        for (String key : source.keySet()) {
+            OriginTrackedValue originTrackedValue = (OriginTrackedValue) source.get(key);
+            map.put(key, originTrackedValue.getValue());
+        }
+
+        String[] names = simpleCommandLinePropertySource.getPropertyNames();
+        for (String name : names) {
+            String value = simpleCommandLinePropertySource.getProperty(name);
+            if (map.containsKey(name)) {
+                System.out.println("参数[" + name + "]被命令行参数覆盖,原值:" + map.get(name) + ";新值:" + value);
+                map.put(name, value);
+            }
+        }
+
         StringBuffer textBuff = new StringBuffer();
         // 格式输出
         for (Map.Entry entry : map.entrySet()) {
